7033de4e96f45a5289059765a11736ede5e42b7f
pedro <pedroschroeder06@gmail.com>
2023-03-09 14:32:48 -0300

    teste

--- PrimeiroProgramaJava.java
+++ PrimeiroProgramaJava.java
@@ -3,5 +3,6 @@ public class PrimeiroProgramaJava
     // método main inicia a execução dp app java
 	public static void main(String[] args){// início do método main
 	    System.out.println("Primeiro Programa em Java!");
+		
 	}// fim do método main
 }// fim da classe PrimeiroProgramaJava
