786f948f4e5ec35c29ff114a1442bc682de57f9b
pedro <pedroschroeder06@gmail.com>
2023-04-16 13:43:44 -0300

    ..

--- 1 bimestre/atividades/atvd04/BalancoTrimestral.java
+++ 1 bimestre/atividades/atvd04/BalancoTrimestral.java
@@ -11,11 +11,12 @@ public class BalancoTrimestral
 {// Início da classe Main
     // Declaracao do método main que inicia a execução do programa
 	public static void main(String[] args){// início do método main
+
 	    double gastosJaneiro = 30000.00;    // declarando e iniciando a var gastos de janeiro 
         double gastosFevereiro = 33030.77;  // declarando e iniciando a var gastos de fevereiro 
         double gastosMarco = 23899.01;     // declarando e iniciando a var gastos de marco 
 
-        double gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco; // somando todos os gastos
+        double gastosTrimestre = gastosJaneiro + gastosFevereiro + gastosMarco; // criando e inicializando a var gastosTrimestre
 
         System.out.println(gastosTrimestre);    // print de todos os gastos 
 	}// fim do método main
