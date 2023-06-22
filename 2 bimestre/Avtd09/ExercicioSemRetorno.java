fd70db6376c44393f28d6e52504dc5d83342311d
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-06-22 16:18:18 -0300

    Update ExercicioSemRetorno.java

--- 2 bimestre/Avtd09/ExercicioSemRetorno.java
+++ 2 bimestre/Avtd09/ExercicioSemRetorno.java
@@ -18,38 +18,41 @@ public class ExercicioSemRetorno{
             aux += i + " "; // adicionando a var aux o elemento i + um espaco " " 
         } //fim loop for
 
-
-
         System.out.println(aux); // exibindo os elementos da lista separados por " "
     } //fim do método imprimirNumeros
 
     //inicio do método somaNumeros:::: 
     public void somaNumeros(int[] list_elem1, int[] list_elem2){ //inicializando os parametros do método
+        // declarando variaveis para 
         int soma = 0; // var para receber a soma do array
-        int[] list_elem3 = new int[10];
-        int cont = 0;
+        int[] list_elem3 = new int[10]; // criando um array de 10 elementos
+        int cont;
         for (int i = 0; i <= 9; i++) { // i ira percorrer o array list_elem
-            cont = list_elem1[i] + list_elem2[i];
+            cont = list_elem1[i] + list_elem2[i]; // fazendo a soma dos array
             soma += cont;// soma ira receber a soma dos elementos
-            list_elem3[i] = soma;
+            list_elem3[i] = cont; // colocando em um array a soma dos array
         }//fim do loop for
         
         System.out.println(soma); // exibe a soma de todos elementos do array
 
+        String aux = ""; // criando e inicializando uma var auxiliar
+        for (int i : list_elem3){ // i ira percorrer o array list_elem
+            aux += i + " "; // adicionando a var aux o elemento i + um espaco " " 
+        } //fim loop for
+        System.out.println(aux);
     }//fim do método somaNumeros
 
     public static void main(String [] args) { //incio do método main
         ExercicioSemRetorno x = new ExercicioSemRetorno();
 
-
         int[] a = {12, 3, 16, 1, 23, 45, 7, 19, 18, 31}; // criando e inicializando um array com 10 elementos
-        int[] b = {65, 13, 1, 8, 12, 9, 23, 2, 15, 6};
+        int[] b = {65, 13, 1, 8, 12, 9, 23, 2, 15, 6}; // criando e inicializando um array com com 10 elementos
         int[] c = new int[10];
 
         x.imprimirNumeros(a, b);   //chama o método imprimirNumeros e utiliza o array como parametro
         x.somaNumeros(a, b); //chama o método somaNumeros e utiliza o array como parametro
 
-        x.imprimirNumeros(c, b); //chama o método imprimir
+        // x.imprimirNumeros(c, b); //chama o método imprimir
 
     }//fim do método main
 } //fim da classe ExercicioSemRetorno
