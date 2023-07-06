f1e16e43dff001d4c91a37961d1f4b4a78d2ccd9
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-07-06 16:54:29 -0300

    Update Emprestimo.java

--- 2 bimestre/Atvd 11/Emprestimo.java
+++ 2 bimestre/Atvd 11/Emprestimo.java
@@ -1,1 +1,18 @@
+public class Emprestimo{
+    public double valorTotal;
+    public double taxaJuros;
+    public int numeroParcelas;
 
+
+    public double proximaParcela(){
+        if (numeroParcelas > 0){
+            double valParcela = valorTotal / numeroParcelas; // valor das parcelas
+            double val = valParcela + (valParcela*taxaJuros);
+
+            numeroParcelas--;
+            return val;
+        } else {
+            return -1;
+        }
+    }
+}
