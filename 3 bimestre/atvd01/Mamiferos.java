90ce10a897233300fdd78730dbf63cdc787800db
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-07-27 17:00:27 -0300

    Update Mamiferos.java

--- 3 bimestre/atvd01/Mamiferos.java
+++ 3 bimestre/atvd01/Mamiferos.java
@@ -2,14 +2,16 @@ public class Mamiferos{
     public String nome;
     public int idade;
 
-    //metodo construtor da classe
-    public Mamiferos(String nomee, int idade){
-        this.nome = nomee;
-        this.idade = idade;
+    public void setNome(String nome){
+        this.nome = nome;
     }
 
     public String getNome(){
         return nome;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
     }
 
     public int getIdade(){
@@ -19,6 +21,4 @@ public class Mamiferos{
     public void emitirSom(){}
 
     public void mover(){}
-
-
 }
