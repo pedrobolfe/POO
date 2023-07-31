664aee662db99b6f441600230edbf11db1c47e5f
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-07-31 14:36:16 -0300

    Create Passaro.java

---
+++ 3 bimestre/atvd01/Passaro.java
@@ -0,0 +1,12 @@
+class Passaro extends Ave {
+    private String cor;
+
+    public Passaro(String nome, int idade, String cor) {
+        super(nome, idade);
+        this.cor = cor;
+    }
+
+    public void construirNinho() {
+        // codigo
+    }
+}
