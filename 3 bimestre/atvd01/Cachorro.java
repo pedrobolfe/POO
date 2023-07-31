73e5c00937e8072f1ae3f7790c15385d405d3ba5
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-07-31 19:20:28 -0300

    Update Cachorro.java

--- 3 bimestre/atvd01/Cachorro.java
+++ 3 bimestre/atvd01/Cachorro.java
@@ -1,16 +1,17 @@
-class Cachorro extends Mamiferos {
-    private String raca;
+class Cachorro extends Mamiferos { //inicio da subclasse cachorro de Mamiferos
+    public String raca; //cria a string privada raca
 
-    public Cachorro(String nome, int idade, String raca) {
-        super(nome, idade);
-        this.raca = raca;
-    }
+    public Cachorro(String nome, int idade, String raca) { //inicio da subclasse Cachorro
+        super(nome, idade);  //acessa nome e idade
+        this.raca = raca; //referencia o atributo raca fora da superclasse
+    } //fim da subclasse cachorro
 
-    public void abanarRabo() {
-        // codigo
-    }
-    
-    public void exibirInformacoes() {
-        System.out.println("Raça: " + raca);
-    }
-}
+    public void abanarRabo() { //inicio do metodo abanarRabo
+        System.out.println("dog abanou o rabo"); //print teste
+    } //fim do metodo abanarRabo
+
+    public void exibirInformacoes() { //inicio do metodo exibirInformaçoes
+        // print das informacoes da classe mamiferos e subclasse dog
+        System.out.println("Nome: " + nome + "\n" + "Raça: " + raca + "\n" + "Idade: " + idade);
+    }//fim do metodo exibirInformacoes
+} //fim da subclasse cachorro
