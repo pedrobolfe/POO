07226e6803a84e6097ef38dbe42f8491440e95cd
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-07-31 14:35:21 -0300

    Create Ave.java

---
+++ 3 bimestre/atvd01/Ave.java
@@ -0,0 +1,12 @@
+class Passaros extends Ave {
+    private String cor;
+
+    public Passaros(String nome, int idade, String cor) {
+        super(nome, idade);
+        this.cor = cor;
+    }
+
+    public void construirNinho() {
+        // codigo
+    }
+}
