939a972be8b1bd1152829eb76db951009c15c018
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-07-31 14:57:35 -0300

    Update Main.java

--- 3 bimestre/atvd01/Main.java
+++ 3 bimestre/atvd01/Main.java
@@ -1,1 +1,22 @@
-aa
+import mamiferos.Cachorros;
+import aves.Passaros;
+
+public class Main {
+    public static void main(String[] args) {
+        // cachorros
+        Cachorros cachorro1 = new Cachorros("doguin", 5, "vira-lata");
+        cachorro1.emitirSom();
+        cachorro1.mover();
+        cachorro1.abanarRabo();
+        cachorro1.exibirInformacoes();
+        System.out.println('');
+
+        // passaros
+        Passaros passaro1 = new Passaros("sebastião", 3, "papaguaio");
+        passaro1.emitirSom();
+        passaro1.voar();
+        passaro1.construirNinho();
+        passaro1.exibirInformacoes();
+        System.out.println('');
+    }
+}
