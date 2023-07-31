33911ceaf6cee48819168e95bed7cba0f72d4231
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-07-31 19:20:16 -0300

    Update Passaro.java

--- 3 bimestre/atvd01/Passaro.java
+++ 3 bimestre/atvd01/Passaro.java
@@ -1,16 +1,17 @@
-class Passaro extends Ave {
-    private String cor;
+class Passaro extends Ave { //inicio da subclasse Passaro de Ave
+    public String cor; //cria o atributo privado: cor
 
-    public Passaro(String nome, int idade, String cor) {
-        super(nome, idade);
-        this.cor = cor;
+    public Passaro(String nome, int idade, String cor) { //inicio da superclasse Passaro
+        super(nome, idade); //acessa nome e idade
+        this.cor = cor; //referencia o atributo cor fora da superclasse
     }
 
-    public void construirNinho() {
-        // codigo
-    }
-    
-    public void exibirInformacoes() {
-        System.out.println("Cor: " + cor);
-    }
-}
+    public void construirNinho() { //inicio do metodo contruirNinho
+        System.out.println("Passaro esta construindo seu ninho"); //print
+    } //fim do metodo contruirNinho
+
+    public void exibirInformacoes() { //inicio do metodo exibirInformacoes
+        // print das informacoes da classe aves e subclasse passaro
+        System.out.println("Nome: " + nome + "\n" + "Cor: " + cor + "\n" + "Idade: " + idade);
+    }// fim do metodo exibirInformacoes
+} // fim da subclasse Passaro
