9e3ffe9335ee847848c64968b1df90c4c7e92630
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-07-31 14:34:34 -0300

    Update Cachorros.java

--- 3 bimestre/atvd01/Cachorros.java
+++ 3 bimestre/atvd01/Cachorros.java
@@ -1,3 +1,12 @@
-public class Cachorro extends Mamiferos{
-    int a;
+class Cachorro extends Mamiferos {
+    private String raca;
+
+    public Cachorro(String nome, int idade, String raca) {
+        super(nome, idade);
+        this.raca = raca;
+    }
+
+    public void abanarRabo() {
+        // codigo
+    }
 }
