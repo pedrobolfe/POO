6d058fa64fb746047bb3344bf6f8a3344757bd25
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-07-31 19:19:43 -0300

    Update Main.java

--- 3 bimestre/atvd01/Main.java
+++ 3 bimestre/atvd01/Main.java
@@ -1,22 +1,20 @@
-import Mamiferos.Cachorro;
-import Ave.Passaro;
-
-public class Main {
-    public static void main(String[] args) {
+public class Main { //inicio da classe Main
+    public static void main(String[] args) { //inicio do metodo main
         // cachorros
-        Cachorro cachorro1 = new Cachorro("doguin", 5, "vira-lata");
-        cachorro1.emitirSom();
-        cachorro1.mover();
-        cachorro1.abanarRabo();
-        cachorro1.exibirInformacoes();
-        System.out.println('');
+        Cachorro mamifero1 = new Cachorro("doguinho", 4, "viralata"); //adiciona uma nova variavel a superclasse Cachorro
+        mamifero1.exibirInformacoes();  //chama o metodo  exibirInformacoes
+        mamifero1.emitirSom();  //chama o metodo emitirSom
+        mamifero1.mover();  //chama o metodo  mover
+        mamifero1.abanarRabo();  //chama o metodo  abanarRabo
+        System.out.println(""); //print vazio para pular uma linha
 
         // passaros
-        Passaro passaro1 = new Passaro("sebastião-papagaio", 3, "verde");
-        passaro1.emitirSom();
-        passaro1.voar();
-        passaro1.construirNinho();
-        passaro1.exibirInformacoes();
-        System.out.println('');
-    }
-}
+        Passaro ave1 = new Passaro("sebastiao (papagaio)", 7, "verde"); //adiciona uma nova variavel a superclasse Passaro
+        ave1.exibirInformacoes(); //chama o metodo exibirInformacoes
+        ave1.emitirSom(); //chama o metodo  emitirSom
+        ave1.voar(); //chama o metodo  voar
+        ave1.construirNinho(); //chama o metodo  construirNinho
+        System.out.println(""); //print vazio para pular uma linha
+
+    }//fim do metodo Main
+}//fim da classe Main
