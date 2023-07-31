706cfb9db9d56c60554435ebca10d5faddc1d7f6
pedrosch <98292262+pedrobolfe@users.noreply.github.com>
2023-07-31 19:20:40 -0300

    Update Ave.java

--- 3 bimestre/atvd01/Ave.java
+++ 3 bimestre/atvd01/Ave.java
@@ -1,22 +1,23 @@
-class Ave{
-    private String nome;
-    private int idade;
+class Ave{ //inicio da classe AVe
+    public String nome; //cria o atributo publico
+    public int idade; //cria o atributo publico
 
-    public Ave(String nome, int idade) {
-        this.nome = nome;
-        this.idade = idade;
-    }
+    public Ave(String nome, int idade) { //inicio da superclasse Aves
+        this.nome = nome; //referencia o atributo nome fora da superclasse
+        this.idade = idade; //referencia o atributo idade fora da superclasse
+    } //fim da superclasse Aves
 
-    public void emitirSom() {
-        // codigo
-    }
+    public void emitirSom() { //inicio do metodo emitirSom
+        System.out.println(" 'barulho' "); //print teste
+    }//fim do metodo emitirSom
 
-    public void voar() {
-        // codigo
-    }
+    public void voar() {//inicio do metodo voar
+        System.out.println(nome  + " esta voando"); //print teste
+    } //fim do metodo voar
+
+    public void exibirInformacoes() { //inicio do metodo exibirInformaçoes
+        System.out.println("Nome: " + nome); //print do nome da ave
+        System.out.println("Idade: " + idade); ///print da idade da ave
+    }//fim do metodo exibirinformaçoes
     
-    public void exibirInformacoes() {
-        System.out.println("Nome: " + nome);
-        System.out.println("Idade: " + idade);
-    }
-}
+} //fim da classe Ave
